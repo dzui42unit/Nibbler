c41c5b270a18f43d08559d8cffc0b76928b7102c
Dmytro Zui <dzui@e1r8p8.unit.ua>
2018-08-27 19:35:24 +0300

    Script update to download and setup sdl2_mixer

--- download_sdl.sh
+++ download_sdl.sh
@@ -14,6 +14,7 @@ ln -f -s ~/.brew/Cellar/sdl2/*/lib/ libs/sdl/sdl2
 
 pkg2="sdl2_image"
 pkg3="sdl2_ttf"
+pkg4="sdl2_mixer"
 
 if brew list -1 | grep -q "^${pkg2}\$"; then
     echo "Package '$pkg2' is installed"
@@ -29,6 +30,14 @@ else
    brew install sdl2 sdl2_ttf
 fi
 
+if brew list -1 | grep -q "^${pkg4}\$"; then
+    echo "Package '$pkg4' is installed"
+else
+   # echo "Package '$pkg4' is not installed"
+   brew install sdl2 sdl2_mixer
+fi
+
+
 
 ln -f -s ~/.brew/Cellar/sdl2_image/*/include/SDL2 libs/sdl/sdl2_image
 ln -f -s ~/.brew/Cellar/sdl2_image/*/lib/ libs/sdl/sdl2_image
@@ -36,5 +45,7 @@ ln -f -s ~/.brew/Cellar/sdl2_image/*/lib/ libs/sdl/sdl2_image
 ln -f -s ~/.brew/Cellar/sdl2_ttf/*/include/SDL2 libs/sdl/sdl2_ttf
 ln -f -s ~/.brew/Cellar/sdl2_ttf/*/lib/ libs/sdl/sdl2_ttf
 
+ln -f -s ~/.brew/Cellar/sdl2_mixer/*/include/SDL2 libs/sdl/sdl2_mixer
+ln -f -s ~/.brew/Cellar/sdl2_mixer/*/lib/ libs/sdl/sdl2_mixer
 
 echo "*** WELL DONE ***"
